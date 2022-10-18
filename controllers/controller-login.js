e9d89c02853a0e864cdfe41dea250266c3745692
kacamata09 <muh.ansharazhari@gmail.com>
2022-10-18 14:59:34 +0700

    tambahkan hashing pada query sql

--- controllers/controller-login.js
+++ controllers/controller-login.js
@@ -6,20 +6,21 @@ module.exports = {
     },
     masuk(requ, resp) {
         const getUser = requ.body
-        const queryCari = 'select * from table_user where user_email = ?'
+        const queryCari = 'select * from table_user where user_email = ? and user_password = SHA2(?, 512)'
 
 
-            koneksi.query(queryCari, getUser.user_email, (err, rows, field) => {
+            koneksi.query(queryCari, [getUser.user_email, getUser.user_password], (err, rows, field) => {
                 if(err) throw err
                 // if (getUser.user_email == rows[0].user_email) {
                     if (rows.length > 0) {
-                        if (rows[0].user_password == getUser.user_password) {
-                            resp.send('selamat anda berhasil login')
-                        } else {
-                            resp.send('password yang anda masukkan salah')
-                        }
+                        // if (rows[0].user_password == getUser.user_password) {
+                        //     resp.send('selamat anda berhasil login')
+                        // } else {
+                        //     resp.send('password yang anda masukkan salah')
+                        // }
+                        resp.send('selamat anda berhasil login')
                 } else {
-                    resp.send('email yang anda masukkan salah')
+                    resp.send('email atau password yang anda masukkan salah')
                 }
                 
             })
--- controllers/controller-register.js
+++ controllers/controller-register.js
@@ -9,9 +9,9 @@ module.exports = {
         })
     },
     tambah(requ, resp) {
-        const querysql = 'insert into table_user set ?'
+        const querysql = 'insert into table_user(user_name, user_email, user_password) values(?,?,SHA2(?,512))'
         let getData = requ.body
-        koneksi.query(querysql, getData, (err, rows, field) => {
+        koneksi.query(querysql, [getData.user_name, getData.user_email, getData.user_password], (err, rows, field) => {
             if(err) throw err
             resp.redirect('/register')
         })
